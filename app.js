475b391b55b4803cc2bd1f28a20c810cc4946a82
jpabon <jpabon>
2024-06-01 20:53:59 -0500

    Puerto

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 // Get the client
 const mysql = require('mysql2/promise');
 const cors = require('cors')
